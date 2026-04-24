1c7adaf12a28abb1ab2377f7180c20fde560524f
yams44300 <yams.sono@gmail.com>
2026-04-24 16:09:49 +0200

    Update bot_crypto_sans_mdp.py

--- bot_crypto_sans_mdp.py
+++ bot_crypto_sans_mdp.py
@@ -37,6 +37,7 @@ creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
 client = gspread.authorize(creds)
 
 sheet = client.open("1Xvzy0NQdSu9UuztJaEqRZFSokPFHZvRFHjDy8_5YtkI").sheet1
+sheet.append_row(["TEST", "OK"])
 
 # 👉 évite spam (1 alerte / 10 min max par coin)
 ALERT_COOLDOWN = 600
