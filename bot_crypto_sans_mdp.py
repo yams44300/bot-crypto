76403b960426dd07855c525d7869d74735a8a44b
yams44300 <yams.sono@gmail.com>
2026-04-27 10:13:19 +0200

    Update bot_crypto_sans_mdp.py

--- bot_crypto_sans_mdp.py
+++ bot_crypto_sans_mdp.py
@@ -74,11 +74,11 @@ while True:
                 previous_prices[market] = price
 
                 # filtre volume (plus souple)
-                if volume < 20000:
+                if volume < 10000:
                    continue
                    
                 # 🎯 VRAI DUMP (court terme)
-                if change_short <= -3 and change_24h < 0 and market not in positions:
+                if change_short <= -1.5 and market not in positions:
 
                    positions[market] = price
 
